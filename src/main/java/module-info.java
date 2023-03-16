732fc31eb945846c6a05969376c04eaa515cde1a
Iliailineya <ilavituk011@gmail.com>
2023-03-16 14:04:09 +0200

    message on

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.example.weatherapp {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.weatherapp to javafx.fxml;
+    exports com.example.weatherapp;
+}
